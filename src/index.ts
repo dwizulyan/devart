707a477f4aea0399d3ff13fb04e5b5ebae8d1c33
dwizulyan <dwizulyanhamid@gmail.com>
2026-02-23 14:11:00 +0700

    Wrapping the main function into try catch block

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { greetings } from "./fncs/greetings.js";
+import * as cl from "@clack/prompts"
 
 async function main() {
   await greetings()
 }
 
 (async () => {
-  main();
+  try {
+    main();
+  }
+  catch (err) {
+    cl.log.error(err instanceof Error ? err.message : "Unknown Error")
+  }
 })()
